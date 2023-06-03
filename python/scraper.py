7d7707baff790e11c293e7efc8c461d69e19ad20
Edmundo Meyer <edmundo.meyer@deltasul.com.br>
2023-06-03 17:05:44 -0300

    implement threading for multiple following http requests

--- python/scraper.py
+++ python/scraper.py
@@ -1,23 +1,36 @@
 from flask import Flask, jsonify
 import httpx
 from selectolax.parser import HTMLParser
+import threading
 
 app = Flask(__name__)
 
 @app.route('/scrape', methods=['GET'])
 def scrape_data():
-    results = [
-        get_data(
-            "Asura",
-            "https://www.asurascans.com/",
-            "a.series",
-        ),
-        get_data(
-            "Flame",
-            "https://flamescans.org/",
-            "div.info > a > div.tt",
-        ),
-    ]
+    # Create a list to hold the threads
+    threads = []
+
+    # Create a function to handle each get_data call in a separate thread
+    def scrape_website(website, url, selector):
+        result = get_data(website, url, selector)
+        results.append(result)
+
+    # Create a list to store the results
+    results = []
+
+    # Start a thread for each website
+    t1 = threading.Thread(target=scrape_website, args=("Asura", "https://www.asurascans.com/", "a.series"))
+    threads.append(t1)
+    t1.start()
+
+    t2 = threading.Thread(target=scrape_website, args=("Flame", "https://flamescans.org/", "div.info > a > div.tt"))
+    threads.append(t2)
+    t2.start()
+
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
+
     return jsonify(results)
 
 def get_data(website, url, selector):
