0b91c120fc22a3ba3a772103c585ddeb4bdb9b23
e-meyer <meyeredy20@gmail.com>
2023-06-27 10:53:38 -0300

    feat: flame scraping only ongoing/complete manhwas (complete might get continued some day)

--- python/websites/flame.py
+++ python/websites/flame.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
 import re
 
-def flame_search_scraper(resp, manhwa_page_url_selector, title_selector, cover_url_selector, chapter_number_selector):
+
+def flame_search_scraper(resp, manhwa_page_url_selector, title_selector, cover_url_selector, chapter_number_selector, status_selector):
     soup = BeautifulSoup(resp.text, 'html.parser')
-    
-    page_url = [element.get('href', '').strip() for element in soup.select(manhwa_page_url_selector)]
+
+    page_url = [element.get('href', '').strip()
+                for element in soup.select(manhwa_page_url_selector)]
 
     title_elements = soup.select(title_selector)
     titles = [title.get_text().strip() for title in title_elements]
@@ -12,21 +14,38 @@ def flame_search_scraper(resp, manhwa_page_url_selector, title_selector, cover_u
     items = soup.select(chapter_number_selector)
     chapters = [item.get_text().strip() for item in items]
 
-    cover_url = [element.get('src', '').strip() for element in soup.select(cover_url_selector)]
+    cover_url = [element.get('src', '').strip()
+                 for element in soup.select(cover_url_selector)]
+
+    status_divs = soup.find_all("div", class_="status")
+    status = []
+
+    for status_div in status_divs:
+        i_tag = status_div.find("i")
+        status.append(i_tag.text)
 
     manhwa_data = []
 
     for i, title in enumerate(titles):
-        data = {
-            "title": title,
-            "page_url": page_url[i],
-            "cover_url": cover_url[i],
-        }
+        if status[i] != "Dropped":
+            cover = cover_url[i]
+            pattern = r"-\d{3}x\d{3}"
 
-        if chapters and i < len(chapters):
-            numbers = re.findall(r'\d+', chapters[i])
-            data["chapters"] = numbers[0] if numbers else ''
+            new_url = re.sub(pattern, "", cover)
 
-        manhwa_data.append(data)
+            data = {
+                "title": title,
+                "page_url": page_url[i],
+                "cover_url": new_url,
+            }
+
+            if new_url != cover_url[i]:
+                data["smaller_cover_url"] = cover_url[i]
+
+            if chapters and i < len(chapters):
+                numbers = re.findall(r'\d+', chapters[i])
+                data["chapters"] = numbers[0] if numbers else ''
+
+            manhwa_data.append(data)
 
     return manhwa_data
