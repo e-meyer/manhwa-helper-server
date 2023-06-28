3c8426455867caf347d886796a905d5d54a82270
e-meyer <meyeredy20@gmail.com>
2023-06-28 09:51:32 -0300

    feat: add one endpoint for each scanlator (scanlator/<scanlator_name>)

--- python/app.py
+++ python/app.py
@@ -2,9 +2,33 @@ import json
 import os
 from flask import Flask, jsonify
 from flask import request
-from bs4 import BeautifulSoup
+
 
 app = Flask(__name__)
+
+
+@app.route('/scanlator/<scanlator_name>', methods=['GET'])
+def get_scanlator_data(scanlator_name):
+    supported_scanlators = ['asura', 'flame', 'luminous', 'reaper']
+
+    search = request.args.get('s')
+
+    if not search:
+        return jsonify({'error': 'Search parameter is required'}), 400
+
+    file_path = os.path.join('data', f'{scanlator_name}.json')
+
+    if scanlator_name.lower() not in supported_scanlators:
+        return jsonify({'error': 'Scanlator not supported'}), 400
+    elif not os.path.exists(file_path):
+        return jsonify({'error': 'Data for scanlator not found'}), 404
+    else:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+
+            manhwa_data = search_titles(search, data)
+
+            return manhwa_data
 
 
 @app.errorhandler(404)
@@ -14,50 +38,6 @@ def not_found(error):
         "message": "Route not found"
     }
     return jsonify(response), 404
-
-
-@app.route('/asura', methods=['GET'])
-def asura():
-    query = request.args.get('query')
-
-    if query is None:
-        response = {
-            "status": 400,
-            "message": "Query parameter is required"
-        }
-        return jsonify(response), 400
-
-    json_data = load_manhwa_data("Asura")
-    found_items = search_titles(query, json_data)
-
-    results = []
-
-    for item in found_items:
-        results.append(item)
-
-    return results
-
-
-@app.route('/flame', methods=['GET'])
-def flame():
-    query = request.args.get('query')
-
-    if query is None:
-        response = {
-            "status": 400,
-            "message": "Query parameter is required"
-        }
-        return jsonify(response), 400
-
-    json_data = load_manhwa_data("Flame")
-    found_items = search_titles(query, json_data)
-
-    results = []
-
-    for item in found_items:
-        results.append(item)
-
-    return results
 
 
 def search_titles(query, data):
