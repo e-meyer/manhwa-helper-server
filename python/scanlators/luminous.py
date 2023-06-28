aba5ac697e126cae5b7ffa71928bdddb152ff06d
e-meyer <meyeredy20@gmail.com>
2023-06-28 17:29:27 -0300

    feat: create luminous scraping logic to not bring dropped titles

--- python/scanlators/luminous.py
+++ python/scanlators/luminous.py
@@ -1,10 +1,13 @@
+import json
 from bs4 import BeautifulSoup
 import re
 
+
 def luminous_search_scraper(resp, manhwa_page_url_selector, title_selector, cover_url_selector, chapter_number_selector):
     soup = BeautifulSoup(resp.text, 'html.parser')
-    
-    page_url = [element.get('href', '').strip() for element in soup.select(manhwa_page_url_selector)]
+
+    page_url = [element.get('href', '').strip()
+                for element in soup.select(manhwa_page_url_selector)]
 
     title_elements = soup.select(title_selector)
     titles = [title.get_text().strip() for title in title_elements]
@@ -12,21 +15,52 @@ def luminous_search_scraper(resp, manhwa_page_url_selector, title_selector, cove
     items = soup.select(chapter_number_selector)
     chapters = [item.get_text().strip() for item in items]
 
-    cover_url = [element.get('src', '').strip() for element in soup.select(cover_url_selector)]
+    cover_url = [element.get('src', '').strip()
+                 for element in soup.select(cover_url_selector)]
+
+    divs = [element
+            for element in soup.select("div.bs > div.bsx > span")]
+
+    dropped_titles = []
+
+    scripts = soup.find_all('script', type="text/javascript")
+    for script in scripts:
+        if 'const dropped' in script.text:
+            js_code = script.text
+            break
+
+    # Step 3: Extract the "dropped" array from the JavaScript code
+    dropped_array_str = re.search(r'const dropped = {[^}]*}', js_code).group()
+
+    # Step 4: Extract the series titles from the dropped_array_str
+    titles_str = re.search(r'\[.*\]', dropped_array_str).group()
+
+    # Step 5: Convert the titles_str to a list
+    titles_str = titles_str[1:-1]  # remove the square brackets
+    dropped_titles = [title.strip()[1:-1] for title in titles_str.split(',')]
 
     manhwa_data = []
 
     for i, title in enumerate(titles):
-        data = {
-            "title": title,
-            "page_url": page_url[i],
-            "cover_url": cover_url[i],
-        }
+        if title not in dropped_titles:
+            cover = cover_url[i]
+            pattern = r"-\d{3}x\d{3}"
 
-        if chapters and i < len(chapters):
-            numbers = re.findall(r'\d+', chapters[i])
-            data["chapters"] = numbers[0] if numbers else ''
+            new_url = re.sub(pattern, "", cover)
 
-        manhwa_data.append(data)
+            data = {
+                "title": title,
+                "page_url": page_url[i],
+                "cover_url": new_url,
+            }
+
+            if new_url != cover_url[i]:
+                data["smaller_cover_url"] = cover_url[i]
+
+            if chapters and i < len(chapters):
+                numbers = re.findall(r'\d+', chapters[i])
+                data["chapters"] = numbers[0] if numbers else ''
+
+            manhwa_data.append(data)
 
     return manhwa_data
