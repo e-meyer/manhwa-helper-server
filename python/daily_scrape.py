f7b878a31e7ccdd9a10c9419ad4c46eb7388f176
e-meyer <meyeredy20@gmail.com>
2023-06-28 17:31:07 -0300

    refactor: remove api calls from daily scraping, added reaper and luminous functions

--- python/daily_scrape.py
+++ python/daily_scrape.py
@@ -1,7 +1,6 @@
 import json
 import os
-from flask import Flask, jsonify
-from flask import request
+from flask import jsonify
 from bs4 import BeautifulSoup
 import requests
 from time import sleep
@@ -11,10 +10,7 @@ from scanlators.flame import flame_search_scraper
 from scanlators.luminous import luminous_search_scraper
 from scanlators.reaper import reaper_search_scraper
 
-app = Flask(__name__)
 
-
-@app.route('/asura')
 def asura():
     page_number = 1
     manhwa_data = []
@@ -57,7 +53,6 @@ def asura():
     return scraped_data
 
 
-@app.route('/flame')
 def flame():
     page_number = 1
     manhwa_data = []
@@ -99,6 +94,47 @@ def flame():
     return scraped_data
 
 
+def reaper():
+    page_number = 1
+    manhwa_data = []
+
+    while True:
+        url = "https://reaperscans.com/comics?page=" + str(page_number)
+
+        response = request_scanlator_data(
+            url,
+            headers={
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0"
+            },
+        )
+
+        data_returned = reaper_search_scraper(
+            response,
+            manhwa_page_url_selector="li > div > a",
+            title_selector="li > div > ",
+            cover_url_selector="li > div > a > img",
+            chapter_number_selector="div.epxs",
+        )
+
+        if len(data_returned) == 0:
+            break
+
+        for item in data_returned:
+            manhwa_data.append(item)
+
+        page_number += 1
+        sleep(10)
+
+    scraped_data = {
+        "scanlator": "Reaper",
+        "manhwa_data": manhwa_data
+    }
+
+    save_manhwa_data("reaper", scraped_data)
+
+    return scraped_data
+
+
 def request_scanlator_data(url, headers):
     try:
         response = requests.get(
@@ -124,58 +160,45 @@ def save_manhwa_data(file_name, data):
         json.dump(data, json_file)
 
 
-@app.errorhandler(404)
-def not_found(error):
-    response = {
-        "status": 404,
-        "message": "Route not found"
+def luminous():
+    page_number = 1
+    manhwa_data = []
+
+    while True:
+        url = "https://www.luminousscans.com/series/?page=" + str(page_number)
+
+        response = request_scanlator_data(
+            url,
+            headers={
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0"
+            },
+        )
+
+        data_returned = luminous_search_scraper(
+            response,
+            manhwa_page_url_selector="div.bsx > a",
+            title_selector="div.bigor > div.tt",
+            cover_url_selector="div.limit > img",
+            chapter_number_selector="div.epxs",
+            status_selector="div.limit > div.status",
+        )
+
+        if len(data_returned) == 0:
+            break
+
+        for item in data_returned:
+            manhwa_data.append(item)
+
+        page_number += 1
+        sleep(10)
+
+    scraped_data = {
+        "scanlator": "Luminous",
+        "manhwa_data": manhwa_data
     }
-    return jsonify(response), 404
 
+    save_manhwa_data("luminous", scraped_data)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5500)
+    return scraped_data
 
-# @app.route('/luminous', methods=['GET'])
-# def luminous():
-#     query = request.args.get('query')
-#     url="https://luminousscans.com/?s=" + query
-
-#     response = request_scanlator_data(
-#         url,
-#         headers={
-#             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0"
-#         },
-#     )
-
-#     manhwa_data = luminous_search_scraper(
-#         response,
-#         manhwa_page_url_selector="div.bsx > a",
-#         title_selector="div.bigor > div.tt",
-#         cover_url_selector="div.limit > img",
-#         chapter_number_selector="div.epxs",
-#     )
-
-#     return {
-#         "scanlator": "Luminous",
-#         "manhwa_data": manhwa_data
-#     }
-
-# @app.route('/reaper', methods=['GET'])
-# def reaper():
-#     query = request.args.get('query')
-#     url="https://reaperscans.com/"
-
-#     response = request_scanlator_data(
-#         url,
-#         headers={
-#             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0"
-#         },
-#         )
-
-#     manhwa_data = reaper_search_scraper(response, query)
-
-#     return {
-#         "scanlator": "Reaper",
-#         "manhwa_data": manhwa_data,
-#     }
+luminous()
