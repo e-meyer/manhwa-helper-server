c237ec31e53b850afccdfeeabd86913e9c8f290a
e-meyer <meyeredy20@gmail.com>
2023-06-30 11:45:38 -0300

    feat: now search queries are better filtered, search for each term queried separately

--- python/app.py
+++ python/app.py
@@ -46,8 +46,10 @@ def not_found(error):
 
 def search_titles(query, data):
     results = []
+    query_words = query.lower().split()
     for item in data["manhwa_data"]:
-        if query.lower() in item["title"].lower():
+        title_words = item["title"].lower().split()
+        if all(any(query_word in title_word for title_word in title_words) for query_word in query_words):
             results.append(item)
     return results
 
